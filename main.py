d5b6d6a7df71f6ae2f43f5bd7645743c9907e149
Soa <hei.soa.3@gmail.com>
2025-09-03 19:40:47 +1300

    FAST API question 1.b

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-from fastapi import FastAPI, Request
 
-
+from fastapi import FastAPI, status
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+from typing import List
 app = FastAPI()
 
 
 @app.get("/health")
 def get_health():
     return {"message": f"OK"}
-   
+
+
+
+class Characteristics(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+class Phone(BaseModel):
+    identifier: str
+    brand: str
+    model: str
+    characteristics: Characteristics
+
+@app.post("/phones", status_code=status.HTTP_201_CREATED)
+def create_phones(phones: List[Phone]):
+    return {
+        "message": "Phones created successfully",
+        "data": phones
+    }
+
+
+
+
